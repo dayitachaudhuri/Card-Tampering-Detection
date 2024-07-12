4f0c1631306f1881cc0914ed7811968bf51b4dbf
dayita <dayitachaudhuri.tech@gmail.com>
2024-07-13 03:42:39 +0530

    Modified views to add necessary backend for image watermarking

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from skimage.metrics import structural_similarity
 import imutils
 import cv2
 from PIL import Image
+import numpy as np
 
 # Adding path to config
 GENERATED_FILE_PATH = 'app/static/generated'
@@ -19,8 +20,8 @@ if not os.path.exists(ORIGINAL_FILE_PATH):
     os.makedirs(ORIGINAL_FILE_PATH)
 
 # Route to get diff
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/image-diff-finder", methods=["GET", "POST"])
+def imageDiffFinder():
     
     if request.method == "GET":
         props = {
@@ -35,10 +36,10 @@ def index():
             original_file_upload = request.files['original_file_upload']
                     
             # Resize and save the uploaded images
-            check_image = Image.open(new_file_upload).resize((250,160))
+            check_image = Image.open(new_file_upload).convert("RGB").resize((250,160))
             check_image.save(os.path.join(NEW_FILE_PATH, 'image.jpg'))
 
-            original_image = Image.open(original_file_upload).resize((250,160))
+            original_image = Image.open(original_file_upload).convert("RGB").resize((250,160))
             original_image.save(os.path.join(ORIGINAL_FILE_PATH, 'image.jpg'))
 
             # Read uploaded and original images as array
@@ -79,6 +80,99 @@ def index():
                 "diff_image": "../static/generated/image_diff.jpg"
             }
             return render_template('image_diff_finder.html', **props)
+        except Exception as e:
+            print(f"Error in Backend: {e}")
+            
+''' 
+Route to Watermark Image.
+
+Path - /image-watermark
+Methods - GET, POST
+'''
+@app.route("/image-watermark", methods=["GET", "POST"])
+def imageWatermark():
+    
+    if request.method == "GET":
+        props = {
+            "show_result" : False
+        }
+        return render_template('image_watermark.html', **props)
+    
+    elif request.method == "POST":
+        try:
+            
+            # Get Watermarking Type from Form
+            watermark_type = request.form['watermark_type']
+            
+            # Open Cover Image.
+            cover_image_ref = request.files['cover_image']
+            if not cover_image_ref:
+                return "Cover image not uploaded", 400
+            cover_image = Image.open(cover_image_ref)
+            
+            # Convert Cover Image to numpy array of required type.
+            cover_image = np.array(cover_image.convert('RGB'))
+            cover_image = cv2.cvtColor(cover_image, cv2.COLOR_RGB2BGR)
+            
+            # CASE 1 - If User Requests for Image Watermark.
+            if watermark_type == "image":
+                
+                # Open Watermarking Image.
+                logo_image_ref = request.files['watermark_image']
+                if not logo_image_ref:
+                    return "Watermark image not uploaded", 400
+                logo_image = Image.open(logo_image_ref)
+                
+                # Convert Watermarking Image to numpy array of required type.
+                logo_image = np.array(logo_image.convert('RGB'))
+                logo_image = cv2.cvtColor(logo_image, cv2.COLOR_RGB2BGR)
+
+                # Find  dimensions of cover image and watermarking image and determine the center of the cover image.
+                h_image, w_image, _ = cover_image.shape
+                h_logo, w_logo, _ = logo_image.shape 
+                
+                center_y = int(h_image / 2)
+                top_y = center_y - int(h_logo / 2)
+                bottom_y = top_y + h_logo
+
+                center_x = int(w_image / 2)
+                left_x = center_x - int(w_logo / 2)
+                right_x = left_x + w_logo   
+
+                # Determine ROI (Region Of Interest) in the cover image where the watermarking image shall be embedded. 
+                # ROI is chosen such that the centers of the Cover Image and Watermarking Image align.
+                roi = cover_image[top_y: bottom_y, left_x: right_x]
+                
+                # Add the watermarking image to the cover image.
+                result = cv2.addWeighted(roi, 1, logo_image, 1, 0)
+                cover_image[top_y: bottom_y, left_x: right_x] = result
+            
+            # CASE 2 - If User Requests for Text Watermark.
+            elif watermark_type == "Text":
+                
+                # Get Watermark Text.
+                watermark_text = request.form["watermark_text"]
+                if not watermark_text:
+                    return "Watermark text not uploaded", 400
+                
+                # Get dimensions of Cover Image
+                h_image, w_image, _ = cover_image.shape
+                
+                # Embed the watermark text in the cover image
+                cv2.putText(cover_image, watermark_text, org=(w_image - len(watermark_text) * 10, h_image - 10), 
+                            fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_4)
+
+            # CASE 3 - If Invalid Watermark Type is received.
+            else:
+                return "Watermark type not supported", 400
+                
+            # Save result image.
+            cv2.imwrite(os.path.join(GENERATED_FILE_PATH, 'watermarked_image.jpg'), cover_image)
+            props = {
+                "show_result" : True,
+                "watermarked_image": "../static/generated/watermarked_image.jpg",
+            }
+            return render_template('image_watermark.html', **props)
         except Exception as e:
             print(f"Error in Backend: {e}")
        
